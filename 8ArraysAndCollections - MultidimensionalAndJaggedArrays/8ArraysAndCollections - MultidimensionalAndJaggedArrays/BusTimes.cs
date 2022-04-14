efbcae98619fa846e8ad8f8d1eb684c487ea3888
Catalin Catrina <catalin.catrina@isidors.com>
2022-04-14 13:04:28 +0300

    joi de la munca

---
+++ 8ArraysAndCollections - MultidimensionalAndJaggedArrays/8ArraysAndCollections - MultidimensionalAndJaggedArrays/BusTimes.cs
@@ -0,0 +1,14 @@
+﻿namespace _8ArraysAndCollections___MultidimensionalAndJaggedArrays
+{
+    public class BusTimes
+    {
+        public string[,] Times { get; }
+        public BusRoute Route { get; }
+
+        public BusTimes(BusRoute route, string[,] times)
+        {
+            Route = route;
+            Times = times;
+        }
+    }
+}
---
+++ 8ArraysAndCollections - MultidimensionalAndJaggedArrays/8ArraysAndCollections - MultidimensionalAndJaggedArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _8ArraysAndCollections___MultidimensionalAndJaggedArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusRouteRepository repo = new BusRouteRepository();
+        }
+    }
+}
