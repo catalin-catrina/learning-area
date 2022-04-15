c8460fd6960932c6874c8f32782343c89bd52c89
Catalin Catrina <catalin.catrina@isidors.com>
2022-04-15 12:05:39 +0300

    vineri de la munca, am terminat cursul 8 si am sarit peste cursul 9 pentru ca e varza. invat despre extension methods in some other way, vad eu

---
+++ 8ArraysAndCollections - LinkedLists/8ArraysAndCollections - LinkedLists/PassengerGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8ArraysAndCollections___LinkedLists
+{
+    public class PassengerGenerator
+    {
+        private static int _count = 0;
+
+        // Random is a type that generates random numbers
+        private static Random _rnd = new Random();
+
+        public static Passenger CreatePassenger()
+        {
+            string destination = _rnd.Next(2) == 0 ? "Lancaster" : "Morecambe";
+            return new Passenger($"Person {++_count}", destination);
+        }
+    }
+}
