7ba3ef6e28bfbb86c6ec73e6df81c66d14fd2e3f
Catalin Catrina <catalin.catrina@isidors.com>
2022-04-15 12:57:58 +0300

    extension methods

---
+++ 9ExtensionMethods/9ExtensionMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _9ExtensionMethods
+{
+    public interface IUnit
+    {
+        double Value { get; }
+    }
+
+    public sealed class TemperatureConverter : IUnit
+    {
+        public double Value { get; }
+        public TemperatureConverter (double Value)
+        {
+            Value = Value;
+        }
+
+        public double ToFahrenheit() => Value * 9 / 5 + 32;
+    }
+}
