3381182fa2eeee72c602149328ab58be0139aee8
Catalin Catrina <catalin.catrina@isidors.com>
2022-05-02 12:52:53 +0300

    de la lucru luni

---
+++ 15OOP/ACM/ACM.BL/Order.cs
@@ -0,0 +1,28 @@
+﻿namespace ACM.BL
+{
+    public class Order
+    {
+        public Order ()
+        {
+
+        }
+        public Order (int id)
+        {
+            OrderId = id;
+        }
+
+        public int OrderId { get; set; }
+
+        // DateTimeOffset type keeps track of date, time and time zone offset. Good for when date can be set in different timezones
+        // allows to correctly compare dates from different timezones (10AM in Detroit =/= 10AM in Paris
+        // ? = nullable type; it can hold the value of DateTimeOffset or null
+        // ? is good for date types as there is no good default value
+        public DateTimeOffset? OrderDate { get; set; }
+        public bool Validate()
+        {
+            bool isValid = true;
+            if (OrderDate == null) isValid = false;
+            return isValid;
+        }
+    }
+}
---
+++ 15OOP/ACM/ACM.BL/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿namespace ACM.BL
+{
+    public class ProductRepository
+    {
+        public Product Retrieve(int productId)
+        {
+            Product prod = new Product(productId);
+
+            if (productId == 2)
+            {
+                prod.ProductName = "Sunflowers";
+                prod.ProductDescription = "lorem ipsum";
+                prod.CurrentPrice = 15.96M;
+            }
+
+            return prod;
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
