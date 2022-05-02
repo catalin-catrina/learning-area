38614a527b7cecb70ab0c756cd3a925da66f8d69
Catalin C <catalincatrina94@yahoo.com>
2022-05-02 19:44:54 +0300

    luni de acasa

--- 15OOP/ACM/ACM.BL/CustomerRepository.cs
+++ 15OOP/ACM/ACM.BL/CustomerRepository.cs
@@ -2,6 +2,15 @@
 {
     public class CustomerRepository
     {
+        // Whenever we initialize a CustomerRepository object, the constructor will build an instance of the AddressRepository class
+        // This establishes a collaborative relationship between the two classes
+        public CustomerRepository() 
+        {
+            _addressRepository = new AddressRepository();   
+        }
+
+        private AddressRepository _addressRepository { get; set; }
+
         public Customer Retrieve(int customerId)
         {
             // In terms of relationship between classes: this is called "collaboration" (use a), because CustomerRepository uses an instance of another unrelated class (Customer) - CustomerRepository uses Customer
@@ -12,6 +21,9 @@
                 customer.EmailAddress = "fbaggins@hobbiton.me";
                 customer.FirstName = "Frodo";
                 customer.LastName = "Baggins";
+
+                // we call ToList() on the returned IEnumerable to return the result as a list
+                customer.AddressList = _addressRepository.RetrieveByCustomerId(customerId).ToList();
             }
 
             return customer;
--- 15OOP/ACM/ACM.BL/Order.cs
+++ 15OOP/ACM/ACM.BL/Order.cs
@@ -2,13 +2,14 @@
 {
     public class Order
     {
-        public Order ()
+        public Order () : this(0)
         {
 
         }
         public Order (int id)
         {
             OrderId = id;
+            OrderItems = new List<OrderItem>();
         }
 
         public int OrderId { get; set; }
@@ -18,6 +19,19 @@
         // ? = nullable type; it can hold the value of DateTimeOffset or null
         // ? is good for date types as there is no good default value
         public DateTimeOffset? OrderDate { get; set; }
+
+        public int CustomerType { get; set; }
+
+        // Composition relationship between Order class and the Customer and Address classes using IDs
+        // There are several advantages to using id properties instead of object properties to establish relationships between classes
+        // It reduces coupling because the class (Order) no longer has a direct reference to its related classes (Customer and Address)
+        // It's more efficient because OrderRepository does not have to load Customer and Address each time it loads an Order
+        public int CustomerId { get; set; }
+        public int ShippingAddressId { get; set; }
+
+        // here we define the composition relationship between Order and OrderItem using references by defining a list of order items
+        public List<OrderItem> OrderItems { get; set; }
+
         public bool Validate()
         {
             bool isValid = true;
