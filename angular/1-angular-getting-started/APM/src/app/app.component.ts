bb41f90d406137ffc8cdae9431881281871b44e5
Catalin Catrina <catalin.catrina@isidors.com>
2022-05-05 13:00:43 +0300

    joi de la birou

--- angular/1-angular-getting-started/APM/src/app/app.component.ts
+++ angular/1-angular-getting-started/APM/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+// this file defines an angular component called AppComponent
+// a component includes a class and a decorator.
 
+// import Component decorator from the angular/core library
+import { Component } from "@angular/core";
+
+// define the Component decorator (template and metadata)
 @Component({
+  // pm = product management (identifier as part of the app, pm because used when created project with angular/cli)
+  // root = root app component (name that represents this component)
+  // selector is the name of the component when we use the directive (custom element) in the HTML
+  // in HTML we use <pm-root></pm-root>
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // template is the view's HTML
+  template: `
+  <div><h1>{{pageTitle}}</h1>
+    <div>MyFirstComponent</div>
+  </div>
+  `
 })
+
+// creating the class of the component to support the view
+// name of the class is also the name of the entire component
+// export keyword makes the class importable by other parts of the app
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = "Acme Product Management";
 }
+
