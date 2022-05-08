b2bf4aebf37fa95f0cfc1da3c96057e37d559ebe
Catalin Catrina <catalin.catrina@outlook.com>
2022-05-08 22:37:07 +0300

    din weekend 2

--- angular/1-angular-getting-started/APM/src/app/app.module.ts
+++ angular/1-angular-getting-started/APM/src/app/app.module.ts
@@ -8,12 +8,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
 
 // we idenfity the class as an angular module by attaching the NgModule decorator and passing in metadata defining the details of this angular module
 @NgModule({
   // define which components belong to this module so that Angular can locate their selectors
   // everything we declare must be imported
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+  ],
   // define external modules we want available to all components that belong to this angular module (angular modules, 3rd party, or our own / custom)
   // here we import BrowserModule which every browser app must import
   imports: [BrowserModule, FormsModule],
