981c7b22f3b0aaa55df7419e92f7ebcf23892f04
Catalin Catrina <catalin.catrina@outlook.com>
2022-05-10 00:11:53 +0300

    luni de acasa

--- angular/1-angular-getting-started/APM/src/app/products/product-list.component.ts
+++ angular/1-angular-getting-started/APM/src/app/products/product-list.component.ts
@@ -2,6 +2,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -29,28 +30,16 @@ export class ProductListComponent implements OnInit {
   filteredProducts: IProduct[] = [];
 
   // products: any[] = [
-  products: IProduct[] = [
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2021',
-      description: '15 gallon capacity rolling',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png',
-    },
-    {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    },
-  ];
+  products: IProduct[] = [];
+
+  // Longer way of injecting a service
+  // private _productService;
+  // constructor(productService: ProductService) {
+  //   this._productService = productService;
+  // }
+
+  // shorter syntax of injecting a service as a dependency
+  constructor(private productService: ProductService) {}
 
   // performFilter() takes in the list filter which is a string and returns the filtered array of IProduct types
   performFilter(filterBy: string): IProduct[] {
@@ -62,15 +51,16 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('In OnInit');
-    this.listFilter = 'cart';
+    // this.listFilter = 'cart';
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 
-  
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
 
   onRatingClicked(message: string): void {
-    this.pageTitle = "Product List " + message;
+    this.pageTitle = 'Product List ' + message;
   }
 }
