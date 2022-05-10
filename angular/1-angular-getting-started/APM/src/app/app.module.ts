ed675a8a48cd4af58db19c94dd43c8ec47fe3ce7
Catalin Catrina <catalin.catrina@isidors.com>
2022-05-10 13:00:28 +0300

    marti la birou

--- angular/1-angular-getting-started/APM/src/app/app.module.ts
+++ angular/1-angular-getting-started/APM/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from '@angular/common/http';
 
 // we idenfity the class as an angular module by attaching the NgModule decorator and passing in metadata defining the details of this angular module
 @NgModule({
@@ -22,7 +23,7 @@ import { StarComponent } from './shared/star.component';
   ],
   // define external modules we want available to all components that belong to this angular module (angular modules, 3rd party, or our own / custom)
   // here we import BrowserModule which every browser app must import
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   // start up component of the app; the start up component should contain the selector we use in the index.html file
   // bootstrap here lists AppComponent as the startup component for our app
   bootstrap: [AppComponent],
