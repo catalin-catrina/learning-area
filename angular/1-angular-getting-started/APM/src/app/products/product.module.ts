2ca620c418425340535ff17809f16347b21596ec
Catalin Catrina <catalin.catrina@isidors.com>
2022-05-23 11:48:06 +0300

    finished angular getting started

--- angular/1-angular-getting-started/APM/src/app/products/product.module.ts
+++ angular/1-angular-getting-started/APM/src/app/products/product.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ConvertToSpacesPipe,
+  ],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        // hooking up the product detail guard to the appropiate route
+        // we add the canActivate property to the route definition for the ProductDetailComponent and set it to an array
+        // in the array we specify each guard we want to execute when activating the route
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent,
+      },
+    ]),
+    SharedModule,
+  ],
 })
-export class ProductModule { }
+export class ProductModule {}
