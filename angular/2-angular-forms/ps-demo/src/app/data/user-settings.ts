9da6854e54a7fb3b67956fc577dd68717d802ff5
Catalin Catrina <catalin.catrina@isidors.com>
2022-05-24 13:01:44 +0300

    marti birou angular forms

---
+++ angular/2-angular-forms/ps-demo/src/app/data/user-settings.ts
@@ -0,0 +1,7 @@
+export interface IUserSettings {
+  name: string | null;
+  emailOffers: boolean | null;
+  interfaceStyle: string | null;
+  subscriptionType: string | null;
+  notes: string | null;
+}
---
+++ angular/2-angular-forms/ps-demo/src/app/user-settings-form/user-settings-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { IUserSettings } from '../data/user-settings';
+
+@Component({
+  selector: 'app-user-settings-form',
+  templateUrl: './user-settings-form.component.html',
+  styleUrls: ['./user-settings-form.component.css'],
+})
+export class UserSettingsFormComponent implements OnInit {
+  originalUserSettings: IUserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null,
+  };
+
+  userSettings: IUserSettings = { ...this.originalUserSettings };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
