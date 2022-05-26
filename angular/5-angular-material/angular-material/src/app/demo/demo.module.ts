dff5a5100820320c7bfc2c0c7698b4edea9e33ae
Catalin Catrina <catalin.catrina@outlook.com>
2022-05-26 21:27:03 +0300

    joi acasa

--- angular/5-angular-material/angular-material/src/app/demo/demo.module.ts
+++ angular/5-angular-material/angular-material/src/app/demo/demo.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { DemoRoutingModule } from './demo-routing.module';
 import { ButtonsComponent } from './buttons/buttons.component';
-
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
-  declarations: [
-    ButtonsComponent
-  ],
-  imports: [
-    CommonModule,
-    DemoRoutingModule
-  ]
+  declarations: [ButtonsComponent],
+  imports: [CommonModule, DemoRoutingModule, MaterialModule],
 })
-export class DemoModule { }
+export class DemoModule {}
