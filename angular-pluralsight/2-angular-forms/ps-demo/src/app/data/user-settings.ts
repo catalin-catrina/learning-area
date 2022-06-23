cfe5f4703d69f3548b612310d144a14ab93df865
Catalin Catrina <catalin.catrina@outlook.com>
2022-06-23 19:12:22 +0300

    deleted empty folder

--- angular-pluralsight/2-angular-forms/ps-demo/src/app/data/user-settings.ts
+++
@@ -1,7 +0,0 @@
-export interface IUserSettings {
-  name: string | null;
-  emailOffers: boolean | null;
-  interfaceStyle: string | null;
-  subscriptionType: string | null;
-  notes: string | null;
-}
