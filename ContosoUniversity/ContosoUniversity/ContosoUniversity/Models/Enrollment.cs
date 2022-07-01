57222a57fdca8d315bb7ec5825d1770ae96c2634
Catalin Catrina <catalin.catrina@isidors.com>
2022-07-01 16:02:08 +0300

    contoso university

--- ContosoUniversity/ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/ContosoUniversity/ContosoUniversity/Models/Enrollment.cs
@@ -1,4 +1,4 @@
-﻿// EF Core interprets a property as a foreign key if it's named <navigation property name><primary key property name>. For example,StudentID is the foreign key for the Student navigation property, since the Student entity's primary key is ID. Foreign key properties can also be named <primary key property name>. For example, CourseID since the Course entity's primary key is CourseID.
+﻿// EF Core interprets a property as a foreign key if it's named <navigation property name><primary key property name>. For example,StudentID is the foreign key for the Student navigation property, since the Student entity's primary key is ID. Foreign key properties can also be named <primary key property name>. For example, CourseID since the Course entity's primary key is CourseID. 
 
 using System.ComponentModel.DataAnnotations;
 
