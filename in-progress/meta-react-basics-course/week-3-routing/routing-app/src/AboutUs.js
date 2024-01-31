27500fff43a37ec1d1d91a2eaaf4fa7bfdea4cbf
Catalin Catrina <catalin.catrina@outlook.com>
2024-01-31 16:36:24 +0200

    added routing app week 3

---
+++ in-progress/meta-react-basics-course/week-3-routing/routing-app/src/AboutUs.js
@@ -0,0 +1,5 @@
+const AboutUs = () => {
+  return <h2>this is about us</h2>;
+};
+
+export default AboutUs;
--- in-progress/meta-react-basics-course/week-3-routing/routing-app/src/App.js
+++ in-progress/meta-react-basics-course/week-3-routing/routing-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
+import Homepage from "./Homepage";
+import AboutUs from "./AboutUs";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav>
+        <Link to="/">Homepage</Link>
+        <Link to="/about-us">About us</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
     </div>
   );
 }
