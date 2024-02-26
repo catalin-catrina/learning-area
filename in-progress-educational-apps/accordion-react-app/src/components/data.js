2439cfb095cab84eb5f7d9d497551c595932ff1b
Catalin Catrina <catalin.catrina@outlook.com>
2024-02-26 18:39:55 +0200

    started preparing for react interviews

---
+++ in-progress-educational-apps/accordion-react-app/src/components/data.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    id: '1',
+    question: 'What are accordion components?',
+    answer:
+      'An accordion component is a user interface element that typically consists of vertically stacked headers that can be expanded or collapsed to reveal or hide content. It is commonly used to display information in a collapsible manner, allowing users to focus on specific sections of content.',
+  },
+
+  {
+    id: '2',
+    question: 'What are they used for?',
+    answer:
+      'An accordion component is used as a user interface element to present a list of items in a compact and organized manner. It typically consists of vertically stacked headers that can be expanded or collapsed to show or hide content associated with each header. This component is beneficial because it allows users to quickly scan through a list and expand only the items they are interested in, making it easier to navigate through the content.',
+  },
+  {
+    id: '3',
+    question: 'Accordion as a musical instrument',
+    answer:
+      'An accordion is a musical instrument that belongs to the family of box-shaped instruments known as free reed aerophones. It is operated by bellows and produces sound as air flows past reeds within the instrument. Accordions are characterized by their ability to combine a melody section (diskant) with accompanying chords, allowing for a versatile range of musical expression.',
+  },
+  {
+    id: '4',
+    question: 'Can I create an accordion component with a different framework?',
+    answer: 'Yes it is of course possible',
+  },
+];
+
+export default data;
