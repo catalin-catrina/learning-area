532aa039d51e31edb95cf703d337772fadb73575
Catalin Catrina <catalin.catrina@outlook.com>
2024-04-15 21:54:52 +0300

    finished accordion app

--- react-apps/accordion-react-app/src/components/Accordion.jsx
+++ react-apps/accordion-react-app/src/components/Accordion.jsx
@@ -1,31 +1,59 @@
-import React, { useState } from "react";
-import data from "../data";
-import "./Accordion.css";
+import React, { useState } from 'react';
+import data from '../data';
+import './Accordion.css';
 
 function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [multiSelected, setMultiSelected] = useState([]);
+  const [multiple, setMultiple] = useState(false);
 
-  const handleSingleSelection = (id) => {
-    id === selected ? setSelected(null) : setSelected(id);
+  const toggleMultiple = () => {
+    setMultiple(prev => !prev);
+    if (multiple) {
+      setMultiSelected([]);
+    } else {
+      setSelected(null);
+    }
+  };
+
+  const handleSingleSelection = currentId => {
+    currentId === selected ? setSelected(null) : setSelected(currentId);
+  };
+
+  const handleMultipleSelection = currentId => {
+    const multiSelectedCopy = [...multiSelected];
+    if (multiSelectedCopy.includes(currentId)) {
+      setMultiSelected(multiSelectedCopy.filter(id => currentId !== id));
+    } else {
+      setMultiSelected([...multiSelectedCopy, currentId]);
+    }
   };
 
   return (
     <div className="wrapper">
-      <button onClick={}>Enable multiple selection</button>
+      <button onClick={toggleMultiple}>Enable multiple selection</button>
       <div className="accordion">
         {data ? (
-          data.map((item) => (
+          data.map(item => (
             <div key={item.id} className="item">
               <div
                 className="title"
-                onClick={() => handleSingleSelection(item.id)}
+                onClick={() =>
+                  multiple
+                    ? handleMultipleSelection(item.id)
+                    : handleSingleSelection(item.id)
+                }
               >
                 <h3>{item.question}</h3>
                 <span>+</span>
               </div>
-              {selected === item.id && (
-                <div className="acc-content">{item.answer}</div>
-              )}
+              {multiple
+                ? multiSelected.includes(item.id) && (
+                    <div className="acc-content">{item.answer}</div>
+                  )
+                : selected === item.id && (
+                    <div className="acc-content">{item.answer}</div>
+                  )}
             </div>
           ))
         ) : (
