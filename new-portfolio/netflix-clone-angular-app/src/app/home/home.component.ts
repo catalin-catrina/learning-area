c0ccfc10bb12378d91d1030d69a0383a8f79f4f2
Catalin Catrina <catalin.catrina@outlook.com>
2024-04-26 22:44:16 +0300

    more progress on the netflix clone

--- new-portfolio/netflix-clone-angular-app/src/app/home/home.component.ts
+++ new-portfolio/netflix-clone-angular-app/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../services/auth.service';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  imports: [HeaderComponent],
 })
 export class HomeComponent {
   user = JSON.parse(sessionStorage.getItem('user')!);
