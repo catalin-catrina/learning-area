25e00198a4e82d9e88f6e70ed0712fa8df2d5d2c
Catalin Catrina <catalin.catrina@outlook.com>
2024-08-17 13:12:05 +0300

    continued fitness tracker angular project

--- new-portfolio/fitness-tracker/src/app/auth/signup/signup.component.ts
+++ new-portfolio/fitness-tracker/src/app/auth/signup/signup.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, NgForm } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FlexLayoutModule],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
