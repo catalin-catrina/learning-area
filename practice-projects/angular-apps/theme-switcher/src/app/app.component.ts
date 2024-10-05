69032bc92553dc48f06a48e25f8da81a6042acc7
Catalin Catrina <catalin.catrina@outlook.com>
2024-10-05 11:31:01 +0300

    Built a tiny theme switcher to clarify an interview question I received

---
+++ practice-projects/angular-apps/theme-switcher/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  switchTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    document.documentElement.setAttribute('data-theme', newTheme);
+  }
+}
