7293600127e8c508dd57dd3ff233dc718749fa27
Catalin Catrina <catalin.catrina@outlook.com>
2024-10-26 13:12:48 +0300

    some quick react recap and progress on css grid course from wes bos

---
+++ in-progress-courses/react-docs/react-docs-project-v5/src/App.jsx
@@ -0,0 +1,12 @@
+import Game from "./Game";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Game />
+    </>
+  );
+}
+
+export default App;
---
+++ in-progress-courses/react-docs/react-docs-project-v5/src/Square.jsx
@@ -0,0 +1,11 @@
+function Square({ value, onSquareClicked }) {
+  return (
+    <>
+      <button className="square" onClick={onSquareClicked}>
+        {value}
+      </button>
+    </>
+  );
+}
+
+export default Square;
