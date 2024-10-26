4fc7abeff91f38d440269eefec59e684c9c57189
Catalin Catrina <catalin.catrina@outlook.com>
2024-10-26 18:30:57 +0300

    finished the mini game

--- interview-prep/javascript-game-v2/script.js
+++ interview-prep/javascript-game-v2/script.js
@@ -1,6 +1,6 @@
-const gameAreaElement = document.querySelector(".game-area");
-const beeElement = document.querySelector(".bee");
-const flowerElements = document.querySelectorAll(".flowers");
+const gameAreaElement = document.querySelector('.game-area');
+const beeElement = document.querySelector('.bee');
+let flowerElements = document.querySelectorAll('.flower');
 
 let beePosition = { x: 100, y: 100 };
 
@@ -20,20 +20,20 @@ for (let i = 0; i < 5; i++) {
   placeFlowers();
 }
 
-document.addEventListener("keydown", (event) => {
+document.addEventListener('keydown', event => {
   const step = 20;
 
   switch (event.key) {
-    case "ArrowUp":
+    case 'ArrowUp':
       beePosition.y -= step;
       break;
-    case "ArrowRight":
+    case 'ArrowRight':
       beePosition.x += step;
       break;
-    case "ArrowDown":
+    case 'ArrowDown':
       beePosition.y += step;
       break;
-    case "ArrowLeft":
+    case 'ArrowLeft':
       beePosition.x -= step;
       break;
   }
@@ -42,12 +42,14 @@ document.addEventListener("keydown", (event) => {
 
   beeElement.style.left = `${beePosition.x}px`;
   beeElement.style.top = `${beePosition.y}px`;
+
+  eatFlowers();
 });
 
 function placeFlowers() {
-  const flowerElement = document.createElement("div");
+  const flowerElement = document.createElement('div');
   gameAreaElement.appendChild(flowerElement);
-  flowerElement.classList.add("flower");
+  flowerElement.classList.add('flower');
 
   const flowerX = Math.floor(
     Math.random() * (gameAreaElement.offsetWidth - 30)
@@ -69,4 +71,41 @@ function keepInBounds() {
     beePosition.y = gameSize.height - beeSize.height;
 }
 
-function eatFlowers() {}
+function eatFlowers() {
+  const beeBoundaries = {
+    top: beePosition.x,
+    left: beePosition.y,
+    right: beePosition.x + beeSize.width,
+    bottom: beePosition.y + beeSize.height,
+  };
+
+  flowerElements = document.querySelectorAll('.flower');
+
+  flowerElements.forEach(flower => {
+    const flowerPosition = {
+      x: Number(flower.style.top.slice(0, -2)),
+      y: Number(flower.style.left.slice(0, -2)),
+    };
+
+    const flowerSize = {
+      width: flower.offsetWidth,
+      height: flower.offsetHeight,
+    };
+
+    const flowerBoundaries = {
+      top: flowerPosition.x,
+      left: flowerPosition.y,
+      right: flowerPosition.x + flowerSize.width,
+      bottom: flowerPosition.y + flowerSize.height,
+    };
+
+    if (
+      beeBoundaries.top <= flowerBoundaries.bottom &&
+      beeBoundaries.left <= flowerBoundaries.right &&
+      beeBoundaries.right >= flowerBoundaries.left &&
+      beeBoundaries.bottom >= flowerBoundaries.top
+    ) {
+      flower.style.display = 'none';
+    }
+  });
+}
