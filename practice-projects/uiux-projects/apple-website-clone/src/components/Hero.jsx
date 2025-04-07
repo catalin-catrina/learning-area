27c172371204ff53c0d4b195323eb1e042b39b30
Catalin Catrina <catalin.catrina@outlook.com>
2025-04-07 19:05:25 +0300

    more work on the ui ux portfolio websites

--- practice-projects/uiux-projects/apple-website-clone/src/components/Hero.jsx
+++ practice-projects/uiux-projects/apple-website-clone/src/components/Hero.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
+import { heroVideo, smallHeroVideo } from "../utils/assets";
 
 function Hero() {
-  return <div>Hero</div>;
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 760 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 760) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleVideoSrcSet);
+
+    return () => {
+      window.removeEventListener("resize", handleVideoSrcSet);
+    };
+  }, []);
+
+  useGSAP(() => {
+    gsap.to("#hero", {
+      opacity: 1,
+      delay: 1.5,
+    });
+  }, []);
+
+  return (
+    <div className="h-svh bg-black relative">
+      <div className="h-5/6 flex justify-center items-center flex-col">
+        <p id="hero" className="hero-title">
+          iPhone 15 Pro
+        </p>
+        <div className="md:w-10/12 w-9/12">
+          <video
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+            className="pointer-events-none"
+          >
+            <source src={videoSrc} type="video/mp4" />
+          </video>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
--- practice-projects/uiux-projects/apple-website-clone/src/components/Highlights.jsx
+++ practice-projects/uiux-projects/apple-website-clone/src/components/Highlights.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Highlights() {
-  return <div>Highlights</div>;
+  return;
 }
 
 export default Highlights;
