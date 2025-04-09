29fad11c1dd267010a0bd167d19d2c4e5cd217dd
Catalin Catrina <catalin.catrina@outlook.com>
2025-04-09 07:17:00 +0300

    more work on portfolio ui ux website

--- practice-projects/uiux-projects/portfolio-ui-ux-1/app/page.tsx
+++ practice-projects/uiux-projects/portfolio-ui-ux-1/app/page.tsx
@@ -1,11 +1,17 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Stack from "@/components/Stack";
+import LogoAnimation from "@/components/LogoAnimation";
+import Portfolio from "@/components/Portfolio";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Stack />
+      <LogoAnimation />
+      <Portfolio />
     </>
   );
 }
