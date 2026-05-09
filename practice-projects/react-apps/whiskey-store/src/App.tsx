902c9e3d5eb96498851cfaa2f961bbcb044e2dd3
Catalin Catrina <catalin.catrina@outlook.com>
2026-05-09 09:10:41 +0300

    Whiskey Store Portfolio App: Added tailwind as a dependency

--- practice-projects/react-apps/whiskey-store/src/App.tsx
+++ practice-projects/react-apps/whiskey-store/src/App.tsx
@@ -1,15 +1,10 @@
-import styles from "./App.module.css";
 import AppRoutes from "./routes";
 import { AuthProvider } from "./shared/context/auth/AuthProvider";
 
 function App() {
   return (
     <AuthProvider>
-      <div className={styles.container}>
-        <div className={styles.content}>
-          <AppRoutes />
-        </div>
-      </div>
+      <AppRoutes />
     </AuthProvider>
   );
 }
