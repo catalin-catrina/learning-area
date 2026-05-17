d2572cc9198bd2b0bf525e8c4f693e76d64404a7
Catalin Catrina <catalin.catrina@outlook.com>
2026-05-17 11:15:51 +0300

    angular interview prep

---
+++ interview-prep/angular-prep/projects/ui-kit/src/lib/card/card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-card',
+  imports: [],
+  templateUrl: './card.html',
+  styleUrl: './card.css',
+})
+export class Card {
+
+}
---
+++ interview-prep/angular-prep/projects/ui-kit/src/lib/input/input.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-input',
+  imports: [],
+  templateUrl: './input.html',
+  styleUrl: './input.css',
+})
+export class Input {
+
+}
